d2b4e31d1581e61704431ef3de09be9d08cdfce6
Konstantin Korolenko <softslot-com@yandex.ru>
2023-02-14 18:15:47 +0300

    add tests for Auth/Profile

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,21 +1,3 @@
 <?php
 
-namespace Tests\Feature;
-
-// use Illuminate\Foundation\Testing\RefreshDatabase;
-use Tests\TestCase;
-
-class ExampleTest extends TestCase
-{
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testApplicationReturnsSuccessfulResponse()
-    {
-        $response = $this->get('/');
-
-        $response->assertOk();
-    }
-}
+test('example')->get('/')->assertStatus(200);
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,12 @@
 
 namespace Tests;
 
+
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
 }
