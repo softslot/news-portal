21717fc820895354e522f07d2a89984d77c9f638
Konstantin Korolenko <softslot-com@yandex.ru>
2023-02-14 21:11:29 +0300

    Revert "add auth components"
    This reverts commit 537d21f9ac7968470c27a97c1da6c7315b4e48a8.

--- resources/views/components/form/button.blade.php
+++
@@ -1,8 +0,0 @@
-<button {{
-            $attributes->merge([
-                'type' => 'submit',
-                'class' => 'text-center w-full bg-blue-900 rounded-md text-white py-3 font-medium',
-            ]) 
-        }}>
-    {{ $slot }}
-</button>
--- resources/views/components/form/link.blade.php
+++
@@ -1,4 +0,0 @@
-<a {{ $attributes->merge([
-        'href' => '#',
-        'class' => 'font-medium text-blue-900 hover:bg-blue-300 rounded-md p-2'
-    ]) }}>{{ $slot }}</a>
