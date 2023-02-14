58cfc04761fc11fd71b9ae14a56b1181422e93c8
Konstantin Korolenko <softslot-com@yandex.ru>
2023-02-14 21:10:57 +0300

    Revert "add tests for Auth/Profile"
    This reverts commit d2b4e31d1581e61704431ef3de09be9d08cdfce6.

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,3 +1,21 @@
 <?php
 
-test('example')->get('/')->assertStatus(200);
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testApplicationReturnsSuccessfulResponse()
+    {
+        $response = $this->get('/');
+
+        $response->assertOk();
+    }
+}
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,9 @@
 
 namespace Tests;
 
-
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-    use RefreshDatabase;
 }
