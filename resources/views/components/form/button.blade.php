537d21f9ac7968470c27a97c1da6c7315b4e48a8
Konstantin Korolenko <softslot-com@yandex.ru>
2023-02-14 18:13:21 +0300

    add auth components

---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,8 @@
+<button {{
+            $attributes->merge([
+                'type' => 'submit',
+                'class' => 'text-center w-full bg-blue-900 rounded-md text-white py-3 font-medium',
+            ]) 
+        }}>
+    {{ $slot }}
+</button>
---
+++ resources/views/layouts/guest.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+        <title>{{ config('app.name', 'Blog') }}</title>
+
+        <!-- Scripts -->
+        @vite(['resources/css/app.css'])
+    </head>
+    <body>
+        <div class="h-screen bg-white flex flex-col space-y-10 justify-center items-center">
+            <div class="bg-white w-96 shadow-xl rounded p-5">
+                {{ $slot }}
+            </div>
+        </div>
+    </body>
+</html>
